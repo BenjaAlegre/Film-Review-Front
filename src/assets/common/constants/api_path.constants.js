29105f0fdaeb9fa5b573a5d5ea1bf454ce0034b4
GonzaLorenzoAdviters <capacitaciones06.06@adviters.com>
2024-07-30 14:35:32 -0300

    Api_path constant fix

--- src/assets/common/constants/api_path.constants.js
+++ src/assets/common/constants/api_path.constants.js
@@ -1,4 +1,4 @@
-export const API_PATH = "http://localhost:3003"
-export const API_PATH_FILMS = "http://localhost:3003/films"
-export const API_PATH_REVIEWS = "http://localhost:3003/reviews"
-export const API_PATH_LOGIN = "http://localhost:3003/login"
+export const API_PATH = "http://localhost:3000"
+export const API_PATH_FILMS = "http://localhost:3000/films"
+export const API_PATH_REVIEWS = "http://localhost:3000/reviews"
+export const API_PATH_LOGIN = "http://localhost:3000/login"
