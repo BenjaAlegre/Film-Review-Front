55a23d28244383fbc4e65671d60137f6a9199cab
BenjaAlegre <CAPACITACIONES06.02@ADVITERS.COM>
2024-07-30 14:18:43 -0300

    fix register and login style

--- src/assets/pages/Register/Register.jsx
+++ src/assets/pages/Register/Register.jsx
@@ -11,9 +11,12 @@ const Register = () => {
     const navigate = useNavigate();
 
 
+
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
+            setRole('User');
           const response = await fetch('http://localhost:3000/users', {
             method: 'POST',
             headers: {
@@ -42,20 +45,20 @@ const Register = () => {
     return (
         <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create account</h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
                 <div>
-                        <label htmlFor="userName" className="block text-sm font-medium leading-6 text-gray-900">User Name</label>
+                        <label htmlFor="userName" className="flex text-sm font-medium leading-6 text-gray-900">Username</label>
                         <div className="mt-2">
                             <input type="userName" value={name} onChange={(e) => setName(e.target.value)} id="userName" name="userName" autoComplete="userName"  required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
                     <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <label htmlFor="email" className="flex text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div className="mt-2">
                           
                             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" autoComplete="email"  required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
@@ -65,23 +68,21 @@ const Register = () => {
                     <div>
                         <div className="flex items-center justify-between">
                             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                            <div className="text-sm">
-                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                            </div>
+
                         </div>
                         <div className="mt-2">
                             <input id="password" value={password} onChange={(e) => setPassword(e.target.value)}  name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
-                    <div>
+                    {/* <div>
                         <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">Role</label>
                         <div className="mt-2">
                             <input type="role" value={role} onChange={(e) => setRole(e.target.value)} id="role" name="role" autoComplete="role"  required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         </div>
-                    </div>
+                    </div> */}
 
                     <div>
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
                     </div>
                 </form>
 
