f0f61edef7b8b5ee120b639346124b29f794a3cd
GonzaLorenzoAdviters <capacitaciones06.06@adviters.com>
2024-07-31 10:46:25 -0300

    film merge

--- src/assets/pages/Film/Film.jsx
+++ src/assets/pages/Film/Film.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { getOneData } from "../../common/utils/getData";
 import DetailedFilm from "./DetailedFilm";
 import FeaturedReview from "./FeaturedReview";
@@ -7,47 +7,39 @@ import { API_PATH_FILMS } from "../../common/constants/api_path.constants";
 import { getAverageReviewScore } from "../../common/utils/getAverageReviewScore";
 import { useLocation } from "react-router-dom";
 
-const Film = ( ) =>
-{
+const Film = () => {
+
     const { state } = useLocation();
 
-    console.log({state});
-    console.log(state);
     const filmID = state;
-    console.log(filmID);
-    // const filmID = "4ce26d3e-1702-4cd1-b334-a8907ecb45ef"
 
     let [film, setFilm] = useState([])
     let [averageScore, setAverageScore] = useState(0);
 
-    useEffect(() =>
-    {
-      getFilm()
-    },[])
+    useEffect(() => {
+        getFilm()
+    }, [])
 
-    const getFilm = async () =>
-    {
+    const getFilm = async () => {
         const filmData = await getOneData(API_PATH_FILMS, filmID)
         setFilm(filmData);
-        if(filmData.reviews)
-        {
+        if (filmData.reviews) {
             const avg = getAverageReviewScore(filmData.reviews)
             setAverageScore(avg);
         }
     }
 
-    if(!film || !film.reviews)
-    {
+    if (!film) {
         return <div>Loading...</div>
     }
 
-    return(
+    return (
         <>
-        <DetailedFilm title={film.title} description={film.description} poster={film.poster} avgScore={averageScore}/>
-        <div>
-        <NewReview film={film.id}/>
-        {film.reviews.length > 0 && <FeaturedReview title={film.reviews[0].title} description={film.reviews[0].description} score={film.reviews[0].score} user={film.reviews[0].user.name} filmID={film.id}/>}
-        </div>
+            <DetailedFilm title={film.title} description={film.description} poster={film.poster} avgScore={averageScore} />
+            <div>
+                <NewReview film={film.id} />
+                {film?.reviews?.length && <FeaturedReview title={film.reviews[0].title} description={film.reviews[0].description} score={film.reviews[0].score} user={film.reviews[0].user.name} filmID={film.id} />}
+            </div>
         </>
     )
 }
