5d99e01147bef3dd5281b080f3602749bc98f4f2
GonzaLorenzoAdviters <capacitaciones06.06@adviters.com>
2024-08-01 10:55:05 -0300

    FeaturedReview update

--- src/assets/pages/Review/CommentList.jsx
+++ src/assets/pages/Review/CommentList.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react"
-import { API_PATH_REVIEWS } from "../../common/constants/api_path.constants"
-import { getOneData } from "../../common/utils/getData"
-
 const CommentList = ({review}) => {
-    
+    console.log(review)
     return (
         <>
             <div>
                 {review.comments.map((comment) => (
                     <div key={comment.id}>
+                        <p>{comment.user.name}</p>
                         <p>{comment.description}</p>
                     </div>
                 ))}
