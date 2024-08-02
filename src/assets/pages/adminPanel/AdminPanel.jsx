730cdfcce31bbf71747bd0a093629940b5196197
Benjamin Alex Alegre <CAPACITACIONES06.02@ADVITERS.COM>
2024-08-02 02:26:44 -0300

    final prof style

--- src/assets/pages/adminPanel/AdminPanel.jsx
+++ src/assets/pages/adminPanel/AdminPanel.jsx
@@ -1,77 +1,51 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getPromise } from "../../common/utils/getData";
-import { API_PATH_DELETEDUSERS, API_PATH_REVIEWS } from "../../common/constants/api_path.constants";
+import { API_PATH_DELETEDREVIEWS, API_PATH_DELETEDUSERS, API_PATH_REVIEWS } from "../../common/constants/api_path.constants";
 import AdminReviewList from "./AdminReviewList";
 import AdminUserList from "./AdminUserList";
 
-const AdminPanel = () =>
-{
+const AdminPanel = () => {
     const [users, setUsers] = useState(null);
     const [reviews, setReviews] = useState(null);
     const [hideUsers, setHideUsers] = useState(false);
 
     const userData = JSON.parse(sessionStorage.getItem('user'));
 
-    useEffect(() =>
-    {
-        if(!users && !reviews)
-        getData()
-    }, [users, reviews])
+    useEffect(() => {
+        if (!users && !reviews) {
+            getData();
+        }
+    }, [users, reviews]);
 
     const getData = async () => {
-        const adminData = await getPromise(API_PATH_REVIEWS, API_PATH_DELETEDUSERS, userData.role);
+        const adminData = await getPromise(API_PATH_DELETEDREVIEWS, API_PATH_DELETEDUSERS, userData.role);
         setReviews(adminData[0]);
         setUsers(adminData[1]);
-    }
+    };
 
-    const handleDataChange = () =>
-    {
+    const handleDataChange = () => {
         getData();
-    }
+    };
 
-    const handleClick = (e) =>
-    {
-        e.preventDefault;
-
+    const handleClick = (e) => {
+        e.preventDefault();
         setHideUsers(!hideUsers);
-    }
+    };
 
     if (!users || !reviews) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     return (
         <>
-            <button onClick={handleClick}>{hideUsers ? 'See review list' : 'See user list'}</button>
+            <button className="flex items-center text-indigo-700 border border-indigo-600 py-2 px-6 gap-2 rounded m-5" onClick={handleClick}>{hideUsers ? 'See review list' : 'See user list'}</button>
             {hideUsers ? (
-                users.map((user) => (
-                    <AdminUserList
-                    key={user.id}
-                    id={user.id}
-                    name={user.name}
-                    role={user.role.description}
-                    email={user.email}
-                    createdAt={user.createdAt}
-                    deletedAt={user.deletedAt}
-                    onUserDelete={handleDataChange}
-                    adminRole={userData.role}/>
-                ))
+                <AdminUserList users={users} onUserDelete={handleDataChange} adminRole={userData.role} />
             ) : (
-                reviews.map((review) => (
-                    <AdminReviewList
-                    key={review.id}
-                    id={review.id}
-                    title={review.title}
-                    description={review.description}
-                    score={review.score}
-                    createdAt={review.createdAt}
-                    deletedAt={review.deletedAt}
-                    onReviewDelete={handleDataChange}/>
-                ))
+                <AdminReviewList reviews={reviews} onReviewDelete={handleDataChange} />
             )}
         </>
     );
-    
-}
+};
 
 export default AdminPanel;
--- src/assets/pages/adminPanel/AdminUserList.jsx
+++ src/assets/pages/adminPanel/AdminUserList.jsx
@@ -1,77 +1,81 @@
+"use client";
+
 import { API_PATH_RESTOREUSER, API_PATH_USERS } from "../../common/constants/api_path.constants";
 
-const AdminUserList = ({id, name, email, role, createdAt, deletedAt, onUserDelete, adminRole }) => {
+const AdminUserList = ({ users, onUserDelete, adminRole }) => {
+    const userData = JSON.parse(sessionStorage.getItem('user'));
+    const currentUserId = userData?.id;
 
-    console.log(deletedAt);
+    const handleClick = async (id, deletedAt) => {
+        try {
+            const apiPath = deletedAt ? API_PATH_RESTOREUSER : API_PATH_USERS;
+            const method = deletedAt ? 'POST' : 'DELETE';
+            const response = await fetch(`${apiPath}/${id}`, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': sessionStorage.getItem('token'),
+                    'role': adminRole
+                },
+                body: JSON.stringify({ id }),
+            });
 
-    const handleClick = async (e) =>
-    {
-        e.preventDefault()
-
-        if(deletedAt)
-        {
-            try {
-                const response = await fetch(`${API_PATH_RESTOREUSER}/${id}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': sessionStorage.getItem('token'),
-                        'role': adminRole
-                    },
-                    body: JSON.stringify({ id }),
-                });
-    
-                console.log(response);
-    
-                if (!response.ok) {
-                    throw new Error("Datos invalidos");
-                }
-    
-                onUserDelete();
-            } catch (error) {
-                console.error(error);
+            if (!response.ok) {
+                throw new Error("Datos invalidos");
             }
+
+            onUserDelete();
+        } catch (error) {
+            console.error(error);
         }
-        else
-        {
-            try {
-                const response = await fetch(`${API_PATH_USERS}/${id}`, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': sessionStorage.getItem('token'),
-                        'role': adminRole
-                    },
-                    body: JSON.stringify({ id }),
-                });
-    
-                console.log(response);
-    
-                if (!response.ok) {
-                    throw new Error("Datos invalidos");
-                }
-    
-                onUserDelete();
-    
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        
-    }
+    };
+    const sortedUsers = [...users].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
 
     return (
-        <div style={{ display: "flex", flexFlow: "row wrap", justifyContent: "space-between" }}>
-            <p>{id}</p>
-            <p>{name}</p>
-            <p>{role}</p>
-            <p>{email}</p>
-            <p>{createdAt}</p>
-            <p>{deletedAt ? deletedAt : '-'}</p>
-
-            <button onClick={handleClick}>{deletedAt ? 'RESTORE' : 'DELETE'}</button>
+        <>
+        <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
+            <table className="w-full table-fixed">
+                <thead>
+                    <tr className="bg-gray-100">
+       
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase">Name</th>
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase">Role</th>
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase">Email</th>
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase truncate">Created At</th>
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase truncate">Deleted At</th>
+                        <th className="w-1/6 py-4 px-6 text-left text-gray-600 font-bold uppercase">Action</th> 
+                    </tr>
+                </thead>
+                <tbody className="bg-white">
+                    {sortedUsers.map((user) => (
+                        <tr key={user.id}>
+                            <td className="py-4 px-6 border-b border-gray-200 truncate">{user.name}</td>
+                            <td className="py-4 px-6 border-b border-gray-200 truncate">{user.role.description}</td>
+                            <td className="py-4 px-6 border-b border-gray-200 truncate">{user.email}</td>
+                            <td className="py-4 px-6 border-b border-gray-200 truncate">{user.createdAt}</td>
+                            <td className="py-4 px-6 border-b border-gray-200 truncate">{user.deletedAt ? user.deletedAt : '-'}</td>
+                            <td className="py-4 px-6 border-b border-gray-200">
+                                {user.id !== currentUserId && (
+                                    <button
+                                        onClick={() => handleClick(user.id, user.deletedAt)}
+                                        className={`px-4 py-2 font-bold text-white rounded  ${
+                                            user.deletedAt ? 'bg-green-500 hover:bg-green-700' : 'bg-red-500 hover:bg-red-700'
+                                        }`}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        {user.deletedAt ? 'RESTORE' : 'DELETE'}
+                                    </button>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
-}
+      
+        </>
+    );
+};
 
 export default AdminUserList;
