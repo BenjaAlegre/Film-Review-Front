8bd9fafbf338c9a9c9840e517da28d6895a7a76d
GonzaLorenzoAdviters <capacitaciones06.06@adviters.com>
2024-08-02 10:58:09 -0300

    AdminReviewList fix

--- src/assets/pages/EditReview/EditReviewForm.jsx
+++ src/assets/pages/EditReview/EditReviewForm.jsx
@@ -63,7 +63,7 @@ const EditReviewForm = ({ reviewID, title, description, score }) => {
                 </div>
                 <div className="mb-5">
                     <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 :text-white">Calificación</label>
-                    <input type="number" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-blue-500 :focus:border-blue-500" name="score" min={1} max={MAX_REVIEW_SCORE} defaultValue={score} />
+                    <input type="number" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white :focus:ring-blue-500 :focus:border-blue-500"     name="score" min={1} max={MAX_REVIEW_SCORE} defaultValue={score} />
                 </div>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Enviar</button>
             </form>
--- src/assets/pages/adminPanel/AdminReviewList.jsx
+++ src/assets/pages/adminPanel/AdminReviewList.jsx
@@ -5,9 +5,10 @@ import { API_PATH_REVIEWS } from "../../common/constants/api_path.constants";
 const AdminReviewList = ({ reviews, onReviewDelete }) => {
     const navigate = useNavigate();
 
-    const handleEdit = (id) => {
+    const handleEdit = (review) => {
+        console.log(review)
         navigate('/editReview', {
-            state: id
+            state: review
         });
     };
 
@@ -58,7 +59,7 @@ const AdminReviewList = ({ reviews, onReviewDelete }) => {
                         <td className="py-2 px-2 sm:px-4 border-b border-gray-200">
                             <div className="flex flex-wrap gap-2">
                                 <button 
-                                    onClick={() => handleEdit(review.id)} 
+                                    onClick={() => handleEdit(review)} 
                                     className="px-4 py-2 font-bold text-white rounded bg-yellow-500 hover:bg-yellow-600 text-sm sm:text-base flex-1">
                                     EDITAR
                                 </button>
